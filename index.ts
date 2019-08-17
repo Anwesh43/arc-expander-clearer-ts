37934909e952d055270810d1796a996c10e6a25a
Anwesh43 <anweshthecool0@gmail.com>
2019-08-18 02:04:42 +0530

    adding ScaleUtil class

--- index.ts
+++ index.ts
@@ -2,3 +2,18 @@ const w : number = window.innerWidth
 const h : number = window.innerHeight
 const scGap : number = 0.05
 const rFactor : number = 10
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+
+    static updateValue(scale : number, dir : number) : number {
+        return scale + dir * scGap
+    }
+}
